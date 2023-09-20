37971f7c2bfa8427b95bfd9e17ede14ef8f76008
byunsp3610@gmail.com <byunsp3610@gmail.com>
2023-09-20 15:25:38 +0900

    feat: Payment 폴더 생성

---
+++ Infantory/Infantory/View/Auction/Payment/PaymentView.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentView.swift
+//  Infantory
+//
+//  Created by 변상필 on 2023/09/20.
+//
+
+import SwiftUI
+
+struct PaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentView()
+}
