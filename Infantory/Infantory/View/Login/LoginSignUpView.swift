3f651d1982c5af8dc1e0663d57050fa33b2f0f9c
Jiyoung Ahn <wl950204@naver.com>
2023-09-21 08:53:39 +0900

    Feat: LoginSignUpView 이름, 휴대폰번호 등 상세요소 추가

--- Infantory/Infantory/View/Login/LoginSignUpView.swift
+++ Infantory/Infantory/View/Login/LoginSignUpView.swift
@@ -9,15 +9,67 @@ import SwiftUI
 
 struct LoginSignUpView: View {
     
-    @State var isFocused: Bool = false
     @State private var text: String = ""
-    @FocusState private var isFocusTextField: Bool
+    @State private var nickNameText: String = ""
+    @State private var nameText: String = ""
+    @State private var phoneNumberText: String = ""
+    @State private var addressText: String = ""
+    @State private var detailAddressText: String = ""
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text("닉네임")
+            Text("*이메일") // 소셜로그인하면서 받아올 예정
             TextField("", text: $text)
                 .overlay(UnderLineOverlay())
+                .padding(.bottom)
+            
+            Text("*닉네임")
+            TextField("", text: $nickNameText)
+                .overlay(UnderLineOverlay())
+                .padding(.bottom)
+            
+            Text("이름")
+            TextField("", text: $nameText)
+                .overlay(UnderLineOverlay())
+                .padding(.bottom)
+            
+            Text("휴대폰 번호")
+            TextField("- 없이 입력", text: $phoneNumberText)
+                .overlay(UnderLineOverlay())
+                .padding(.bottom)
+            
+            VStack(alignment: .leading) {
+                Text("우편 번호") // 우편번호 검색 버튼 만들 예정
+                TextField("우편 번호를 검색하세요", text: $text)
+                    .overlay(UnderLineOverlay())
+                    .padding(.bottom)
+                
+                Text("주소")
+                TextField("우편 번호 검색 후, 자동입력 됩니다.", text: $addressText)
+                    .overlay(UnderLineOverlay())
+                    .padding(.bottom)
+                
+                Text("상세주소")
+                TextField("건물, 아파트, 동/호수 입력", text: $detailAddressText)
+                    .overlay(UnderLineOverlay())
+                    .padding(.bottom)
+                
+            }
+            
+            HStack {
+                Spacer()
+                Button {
+                    //
+                } label: {
+                    Text("가입하기")
+                        .frame(width: 330, height: 40, alignment: .center)
+                        .foregroundColor(.white)
+                        .background(.gray)
+                        .cornerRadius(5)
+                }
+                Spacer()
+            }
+
         }
         .padding()
         .navigationTitle("회원가입")
