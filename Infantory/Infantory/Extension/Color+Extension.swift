85031bcd096dcd03317713b41c87dcf20a2c48c8
seonghun <96768451+Ggurup@users.noreply.github.com>
2023-09-21 23:24:06 +0900

    Seonghun (#14)
    * feat: colorAsset, Color 타입 프로퍼티 추가
    
    * feat: font 타입프로퍼티 추가
    
    * feat: tabView 추가, 시작화면 TabView로 변경

---
+++ Infantory/Infantory/Extension/Color+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extension.swift
+//  Infantory
+//
+//  Created by 김성훈 on 2023/09/21.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let infanRed = Color("Red")
+    static let infanDarkGray = Color("DarkGray")
+    static let infanLightGray = Color("LightGray")
+    static let infanSky = Color("Sky")
+    static let infanMain = Color("Blue")
+    static let infanOrange = Color("Orange")
+    static let infanYellow = Color("Yellow")
+}
---
+++ Infantory/Infantory/View/Auction/AuctionMainView.swift
@@ -0,0 +1,20 @@
+//
+//  AuctionMainView.swift
+//  Infantory
+//
+//  Created by 김성훈 on 2023/09/21.
+//
+
+import SwiftUI
+
+struct AuctionMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuctionMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuctionMainView()
+    }
+}
