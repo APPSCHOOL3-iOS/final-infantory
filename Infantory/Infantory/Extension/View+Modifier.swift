d68c292a6dea4405bd450123509e8b56a0348ef3
seonghun <96768451+Ggurup@users.noreply.github.com>
2023-09-22 11:23:33 +0900

    Sh (#17)
    * Feat: 뷰 익스텐션 분리, InfanNavigationBar추가
    
    * Feat: Firebase 패키지 추가, 연동코드 추가

---
+++ Infantory/Infantory/Extension/View+Modifier.swift
@@ -0,0 +1,14 @@
+//
+//  뷰와 관련된 Modifier를 정의하는 공간
+//
+//
+//  Created by 김성훈 on 2023/09/22.
+//
+import SwiftUI
+
+extension View {
+    /// MainView들을 제외한 View 에서 사용가능한 공통 네비게이션 바 수정자
+    func infanNavigationBar(title: String) -> some View {
+        modifier(InfanNavigationBar(title: title))
+    }
+}
--- Infantory/Infantory/InfantoryApp.swift
+++ Infantory/Infantory/InfantoryApp.swift
@@ -5,13 +5,25 @@
 //  Created by 김성훈 on 2023/09/20.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
-struct InfantoryApp: App {   
+struct InfantoryApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             MainTabView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+      FirebaseApp.configure()
+
+    return true
+  }
 }
