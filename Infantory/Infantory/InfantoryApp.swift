baf7694f3b99834ddb3a130fcbfb0547a14dc178
Jiyoung Ahn <wl950204@naver.com>
2023-09-22 14:21:31 +0900

    Feat: Firebase SDK 추가, Kakao 로그인 SDK 추가

--- Infantory/Infantory/InfantoryApp.swift
+++ Infantory/Infantory/InfantoryApp.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
-struct InfantoryApp: App {   
+struct InfantoryApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             MainTabView()
         }
     }
+    
+    init() {
+            // Kakao SDK 초기화
+            KakaoSDK.initSDK(appKey: "45ce2063d86a5a5c18e38528aae46993")
+        }
 }
