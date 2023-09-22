b0f606e1aa7cb4850f434b39fe959b40fd3b4e4a
Jiyoung Ahn <wl950204@naver.com>
2023-09-22 15:05:05 +0900

    Feat: LoginStore 파일 추가

---
+++ Infantory/Infantory/ViewModel/LoginStore.swift
@@ -0,0 +1,17 @@
+//
+//  LoginStore.swift
+//  Infantory
+//
+//  Created by 안지영 on 2023/09/22.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+import KakaoSDKAuth
+import KakaoSDKUser
+
+final class LoginStore: ObservableObject {
+
+}
