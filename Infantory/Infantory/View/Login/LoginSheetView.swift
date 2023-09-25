cc10138c2d556cb6587a22bdfa2d986b6ff5abad
Jiyoung Ahn <wl950204@naver.com>
2023-09-25 16:03:19 +0900

    Refact: sign in 에서 회원가입 여부 확인 후 view 이동 시도..

--- Infantory/Infantory/View/Login/LoginSheetView.swift
+++ Infantory/Infantory/View/Login/LoginSheetView.swift
@@ -10,77 +10,81 @@ import SwiftUI
 struct LoginSheetView: View {
     
     @EnvironmentObject private var loginStore: LoginStore
-    @State private var isLoginSuccess: Bool = false
+    
+    @State private var isShowingSignUp = false
+    @State private var isShowingMainView = false
+    
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(Gradient(colors: [.blue, .purple]))
-                .ignoresSafeArea()
-            VStack {
-                Text("W E L C O M E").font(.headline.bold())
-                    .foregroundColor(.indigo)
-                Spacer().frame(height: 30)
-                Text("로그인 후").font(.largeTitle.bold())
-                Text("이용해주세요.").font(.largeTitle.bold())
-                Spacer().frame(height: 30)
-                Group {
-                    Button {
-                        Task {
-                            await loginStore.kakaoAuthSignIn { _ in
-                                isLoginSuccess.toggle()
-                            }
-                        }
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10)
-                                .frame(width: 330, height: 50)
-                                .foregroundColor(.yellow)
+        NavigationStack {
+            ZStack {
+                Rectangle()
+                    .fill(Gradient(colors: [.blue, .purple]))
+                    .ignoresSafeArea()
+                VStack {
+                    Text("W E L C O M E").font(.headline.bold())
+                        .foregroundColor(.indigo)
+                    Spacer().frame(height: 30)
+                    Text("로그인 후").font(.largeTitle.bold())
+                    Text("이용해주세요.").font(.largeTitle.bold())
+                    Spacer().frame(height: 30)
+                    Group {
+                        Button {
+                            loginStore.kakaoAuthSignIn()
                             
-                            HStack(alignment: .center) {
-                                Image("kakaoLogo")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 35)
-                                Text("카카오로 계속하기")
-                                    .font(.subheadline)
-                                    .fontWeight(.bold)
-                                    .padding(.leading, -5)
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: 330, height: 50)
+                                    .foregroundColor(.yellow)
+                                
+                                HStack(alignment: .center) {
+                                    Image("kakaoLogo")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 35)
+                                    Text("카카오로 계속하기")
+                                        .font(.subheadline)
+                                        .fontWeight(.bold)
+                                        .padding(.leading, -5)
+                                }
                             }
                         }
+                        .buttonStyle(.plain)
+                        
+                        Button {
+                            // login 기능 구현
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: 330, height: 50)
+                                    .foregroundColor(.black)
+                                
+                                HStack(alignment: .center) {
+                                    Image("appleLogo")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 27)
+                                        .clipShape(Circle())
+                                    Text("Apple로 계속하기")
+                                        .font(.subheadline)
+                                        .foregroundColor(.white)
+                                        .fontWeight(.bold)
+                                        .padding(.leading, -5)
+                                }
+                            }
+                        }
+                        .buttonStyle(.plain)
                     }
-                    .buttonStyle(.plain)
                     
-                    Button {
-                        // login 기능 구현
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 10)
-                                .frame(width: 330, height: 50)
-                                .foregroundColor(.black)
-
-                            HStack(alignment: .center) {
-                                Image("appleLogo")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 27)
-                                    .clipShape(Circle())
-                                Text("Apple로 계속하기")
-                                    .font(.subheadline)
-                                    .foregroundColor(.white)
-                                    .fontWeight(.bold)
-                                    .padding(.leading, -5)
-                            }
-                        }
-                    }
-                    .buttonStyle(.plain)
                 }
-                
             }
-        }
-        
-        .presentationDetents([.height(UIScreen.main.bounds.height * 0.7)])
-        .fullScreenCover(isPresented: $isLoginSuccess) {
-            LoginSignUpView()
+            .presentationDetents([.height(UIScreen.main.bounds.height * 0.7)])
+            .fullScreenCover(isPresented: $isShowingSignUp) {
+                LoginSignUpView()
+            }
+            .navigationDestination(isPresented: $isShowingMainView) {
+                MainTabView()
+            }
         }
     }
 }
--- Infantory/Infantory/ViewModel/LoginStore.swift
+++ Infantory/Infantory/ViewModel/LoginStore.swift
@@ -15,13 +15,16 @@ import SwiftUI
 
 final class LoginStore: ObservableObject {
     
+    @Published var isShowingSignUp = false
+    @Published var isShowingMainView = false
+    
     @Published var email: String = ""
     @Published var userName: String = ""
     @Published var password: String = ""
     
     @Published var signUpUser: SignUpUser = SignUpUser(id: "", name: "", phoneNumber: "", loginType: .kakao, address: Address(fullAddress: ""), applyTicket: [ApplyTicket(userId: "", date: Date(), ticketGetAndUse: "회원가입", count: 5)], password: "")
     
-    func kakaoAuthSignIn(completion: @escaping (Bool) -> Void) async {
+    func kakaoAuthSignIn() {
         if AuthApi.hasToken() { // 발급된 토큰이 있는지
             UserApi.shared.accessTokenInfo { _, error in // 해당 토큰이 유효한지
                 if error != nil { // 에러가 발생했으면 토큰이 유효하지 않다.
@@ -33,13 +36,12 @@ final class LoginStore: ObservableObject {
         } else { // 만료된 토큰
             self.openKakaoService()
         }
-        completion(true)
     }
     
     func openKakaoService() {
         if UserApi.isKakaoTalkLoginAvailable() { // 카카오톡 앱 이용 가능한지
             UserApi.shared.loginWithKakaoTalk { oauthToken, error in // 카카오톡 앱으로 로그인
-                if let error = error { // 로그인 실패 -> 종료
+                if error != nil { // 로그인 실패 -> 종료
                     print("Kakao Sign In Error: 카카오앱 실행에 실패했습니다.")
                     return
                 }
@@ -49,7 +51,7 @@ final class LoginStore: ObservableObject {
             }
         } else { // 카카오톡 앱 이용 불가능한 사람
             UserApi.shared.loginWithKakaoAccount { oauthToken, error in // 카카오 웹으로 로그인
-                if let error = error { // 로그인 실패 -> 종료
+                if error != nil { // 로그인 실패 -> 종료
                     print("Kakao Sign In Error: 카카오 계정 로그인에 실패했습니다.")
                     return
                 }
@@ -63,7 +65,6 @@ final class LoginStore: ObservableObject {
         UserApi.shared.me { kakaoUser, error in
             if error != nil {
                 print("카카오톡 사용자 정보 불러오는데 실패했습니다.")
-                
                 return
             }
             guard let email = kakaoUser?.kakaoAccount?.email else { return }
@@ -72,20 +73,31 @@ final class LoginStore: ObservableObject {
             self.password = String(password)
             guard let userName = kakaoUser?.kakaoAccount?.profile?.nickname else { return }
             self.userName = userName
-            
+            // 로그인이 되는지 안되는지 확인하는 함수 -> 에러? 로그인이안된다 -> 회원가입이 안되어있다 -> 회원가입뷰로
+            // 로그인이 된다 -> 메인뷰로
+            self.emailAuthSignIn(email: email, password: String(password), completion: { result in
+                print("컴플리션 값: \(result)")
+                if result {
+                    self.isShowingMainView = true
+                } else {
+                    self.isShowingSignUp = true
+                }
+            })
         }
     }
     
-    func emailAuthSignIn(email: String, password: String) {
+    func emailAuthSignIn(email: String, password: String, completion: @escaping (Bool) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) { result, error in
-            if let error = error {
+            if error != nil {
                 print("error: 로그인에 실패했습니다.")
-                
-                return
+                completion(false)
+                return // 실패하면 회원가입 화면
             }
             
             if result != nil {
-                print("사용자 이메일: \(String(describing: result?.user.email))")
+                print("로그인 성공! 사용자 이메일: \(String(describing: result?.user.email))")
+                // 성공하면 메인화면
+                completion(true)
             }
         }
     }
@@ -93,7 +105,7 @@ final class LoginStore: ObservableObject {
     func emailAuthSignUp(email: String, password: String, completion: (() -> Void)?) {
         
         Auth.auth().createUser(withEmail: email, password: password) { result, error in
-            if let error = error {
+            if error != nil {
                 print("error: 이미 등록되어있는 사용자입니다.")
             }
             if result != nil {
@@ -106,7 +118,13 @@ final class LoginStore: ObservableObject {
     
     func signUpToFirebase() {
         self.emailAuthSignUp(email: self.email, password: "\(self.password)") {
-            self.emailAuthSignIn(email: self.email, password: "\(self.password)")
+            self.emailAuthSignIn(email: self.email, password: "\(self.password)", completion: { result in
+                if result {
+                    self.isShowingMainView = true
+                } else {
+                    self.isShowingSignUp = true
+                }
+            })
         }
     }
 }
