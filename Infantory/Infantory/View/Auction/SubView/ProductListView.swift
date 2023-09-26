a9cbb750da1957549947949c1f3debb1bb7147dd
SeongHun Kim <403shk@naver.com>
2023-09-26 17:16:10 +0900

    Feat: 경매 메인뷰 작업

--- Infantory/Infantory/View/Auction/SubView/ProductListView.swift
+++ Infantory/Infantory/View/Auction/SubView/ProductListView.swift
@@ -13,91 +13,109 @@ struct ProductListView: View {
     @State private var heartButton: Bool = false
     
     var body: some View {
-        List {
-            ForEach(auctionViewModel.auctionProduct) { product in
-                NavigationLink {
-                    AuctionDetailView(userViewModel: userViewModel, auctionProductViewModel: auctionViewModel)
-                } label: {
-                    VStack {
-                        // 배열의 첫번째 값 넣어둠.
-                        HStack {
-                            Image(systemName: "person.circle.fill")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 35)
-                            Text("\(userViewModel.user.name)")
-                            Spacer()
-                            Button(action: {
-                                heartButton.toggle()
-                            }, label: {
-                                Image(systemName: heartButton ? "heart.fill" : "heart")
+        ScrollView {
+            LazyVStack {
+                ForEach(auctionViewModel.auctionProduct) { product in
+                    ZStack {
+                        NavigationLink(destination: AuctionDetailView(userViewModel: userViewModel, auctionProductViewModel: auctionViewModel)) {
+                            EmptyView()
+                        }
+                        .buttonStyle(.plain)
+                        
+                        VStack {
+                            // 배열의 첫번째 값 넣어둠.
+                            HStack {
+                                Image(systemName: "person.circle.fill")
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
-                                    .frame(width: 25)
-                                    .foregroundColor(.infanDarkGray)
-                            })
-                        }
-                        .padding([.horizontal, .top])
-                        VStack(alignment: .leading, spacing: 10) {
-                            HStack {
-                                if product.productImageURLStrings.count > 0 {
-                                    Image("\(product.productImageURLStrings[0])")
+                                    .frame(width: 50, height: 50)
+                                    .cornerRadius(25)
+                                
+                                Text("\(userViewModel.user.name)")
+                                    .font(.infanHeadline)
+                                
+                                Spacer()
+                                Button(action: {
+                                    heartButton.toggle()
+                                }, label: {
+                                    Image(systemName: heartButton ? "heart.fill" : "heart")
                                         .resizable()
                                         .aspectRatio(contentMode: .fit)
-                                        .frame(width: 150, height: 160)
-                                } else {
-                                    EmptyView()
-                                        .frame(width: 150, height: 160)
-                                }
-                                VStack(alignment: .leading, spacing: 10) {
-                                    HStack {
-                                        Text("Hot")
-                                            .font(.infanFootnote)
-                                            .frame(width: 40, height: 20)
-                                            .foregroundColor(.infanDarkGray)
-                                            .background(Color.infanRed)
-                                            .cornerRadius(10)
-                                        Text("New")
-                                            .font(.infanFootnote)
-                                            .frame(width: 40, height: 20)
-                                            .foregroundColor(.infanDarkGray)
-                                            .background(Color.infanGreen)
-                                            .cornerRadius(10)
-                                    }
-                                    VStack(alignment: .leading){
-                                        Text("상품명: \(product.productName)")
-                                            .font(.infanTitle2)
-                                            .foregroundColor(.infanDarkGray)
-                                            .padding(.bottom)
-                                        Text("남은시간: 03:02:01")
-                                            .font(.infanBody)
-                                            .foregroundColor(.infanDarkGray)
-                                        Text("현재 입찰가: \(product.winningPrice ?? 0)")
-                                            .font(.infanBody)
-                                            .foregroundColor(.infanDarkGray)
-                                    }
-                                }
+                                        .frame(width: 25)
+                                        .foregroundColor(.infanDarkGray)
+                                })
                             }
+                            .infanHorizontalPadding()
+                         
+                            VStack(alignment: .leading, spacing: 16) {
+                                HStack {
+                                    if product.productImageURLStrings.count > 0 {
+                                        Image("\(product.productImageURLStrings[0])")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 150, height: 160)
+                                    } else {
+                                        Image("appleLogo")
+                                            .resizable()
+                                            .frame(width: 150, height: 160)
+                                    }
+                                    
+                                    VStack(alignment: .leading, spacing: 10) {
+                                        HStack {
+                                            Text("Hot")
+                                                .font(.infanFootnote)
+                                                .frame(width: 40, height: 20)
+                                                .foregroundColor(.infanDarkGray)
+                                                .background(Color.infanRed)
+                                                .cornerRadius(10)
+                                            Text("New")
+                                                .font(.infanFootnote)
+                                                .frame(width: 40, height: 20)
+                                                .foregroundColor(.infanDarkGray)
+                                                .background(Color.infanGreen)
+                                                .cornerRadius(10)
+                                        }
+                                        VStack(alignment: .leading) {
+                                            Text("상품명: \(product.productName)")
+                                                .font(.infanTitle2)
+                                                .foregroundColor(.infanDarkGray)
+                                            Text("남은시간: 03:02:01")
+                                                .font(.infanBody)
+                                                .foregroundColor(.infanDarkGray)
+                                            Text("현재 입찰가: \(product.winningPrice ?? 0)")
+                                                .font(.infanBody)
+                                                .foregroundColor(.infanDarkGray)
+                                        }
+                                    }
+                                }
+                                .infanHorizontalPadding()
+                                
+                            }
+                            Rectangle()
+                                .fill(Color.infanLightGray)
+                                .frame(height: 2)
                         }
                     }
                 }
             }
+            .padding(.vertical)
+            .onAppear {
+                Task {
+                    do {
+                        try await auctionViewModel.fetchAuctionProducts()
+                    }
+                    catch {
+                        
+                    }
+                }
         }
-        .listStyle(.plain)
-        .onAppear {
-            Task {
-                do {
-                    try await auctionViewModel.fetchAuctionProducts()
-                }
-                catch {
-                    
-                }
-            }
         }
     }
 }
 struct ProductListView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductListView(userViewModel: UserViewModel(), auctionViewModel: AuctionProductViewModel())
+        NavigationStack {
+            ProductListView(userViewModel: UserViewModel(), auctionViewModel: AuctionProductViewModel())
+        }
     }
 }
