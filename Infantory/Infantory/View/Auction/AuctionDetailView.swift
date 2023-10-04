664268bcd3082f9b6b38c8cb1c81b8ac7cdd06a9
sangpil byeon <byunsp3610@gmail.com>
2023-10-04 13:07:57 +0900

    Sangpil (#54)
    * Feat: 경매 진행 페이지 현재 입찰자 배너 추가
              경매 진행 페이지(AuctionDetailView) 뷰 기본 틀 구성
    
    * Feat: 경매 진행 페이지 및 입찰 시트 뷰 생성
    
    * Style: 경매 진행 뷰 포맷팅

--- Infantory/Infantory/View/Auction/AuctionDetailView.swift
+++ Infantory/Infantory/View/Auction/AuctionDetailView.swift
@@ -12,45 +12,94 @@ struct AuctionDetailView: View {
     @ObservedObject var auctionProductViewModel: AuctionProductViewModel
     
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "person.circle.fill")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 35)
-                Text("\(userViewModel.user.name)")
-                Spacer()
+        ZStack(alignment: .bottom) {
+            ScrollView(showsIndicators: false) {
+                // 인플루언서 프로필
+                
+                
+                Divider()
+                
+                AuctionBuyerView()
+                
+                
+                AuctionItemImage()
+                    .frame(width: .screenWidth - 40, height: .screenWidth - 40)
+                    .cornerRadius(8)
+                
                 
                 HStack {
-                    Image(systemName: "ticket")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 25, height: 25)
-                    Text(": \(userViewModel.user.applyTicket?[0].count ?? 0)")
-                        .font(.infanBody)
-                        .bold()
-                }
-            }
-            Divider()
-                .padding([.horizontal, .top])
-            
-            Section {
-                HStack {
-                    Text("이런 상품들은 어때요?")
+                    Text("멋쟁이 신발")
+                        .font(.infanTitle2)
                     Spacer()
+                    // 남은 시간
+                    Label("03:22:15", systemImage: "timer")
+                        .foregroundColor(.infanMain)
+                        .font(.infanFootnote)
                 }
-                VStack {
-                    AuctionScrollImageView()
-                }
+                .padding(.top)
+                .infanHorizontalPadding()
+                
+                // 제품 설명
+                Text("제품 설명 최대 3줄로 하고 리딩 정렬하고 3줄 넘으면 ...으로 보이게 하rl제품 설명 최대 3줄로 하고 리딩 정렬하고 3줄 넘으면 ...으로 보이게 하rl제품 설명 최대 3줄로 하고 리딩 정렬하고 3줄 넘으면 ...으로 보이게 하rl")
+                //                    .lineLimit(3)
+                    .infanHorizontalPadding()
+                    .padding(.top)
+                    .padding(.bottom, 100)
             }
-            AuctionDetailImageView(auctionProductVIewModel: auctionProductViewModel)
-            AuctionDetailDescriptionView(auctionProductViewModel: auctionProductViewModel)
+            
+            Footer()
+            
         }
+        
+    }
+}
+
+// MARK: - 경매하기 버튼 Footer
+struct Footer: View {
+    
+    @State private var isShowingAuctionBidSheet: Bool = false
+    
+    var body: some View {
+        VStack {
+            Button {
+                isShowingAuctionBidSheet.toggle()
+            } label: {
+                Text("입찰 10000원")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .background(
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(Color.infanMain)
+                            .frame(width: CGFloat.screenWidth - 40, height: 54)
+                    )
+            }
+            .offset(y: -20)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .frame(height: 110)
+        .background(
+            Rectangle()
+                .stroke(lineWidth: 0.1)
+                .background(.white)
+            
+        )
+        .offset(x: 0, y: 40)
+        .sheet(isPresented: $isShowingAuctionBidSheet, content: {
+            AuctionBidSheetView()
+                .presentationDragIndicator(.visible)
+                .presentationDetents([.medium])
+            
+            
+            
+        })
     }
 }
 
 struct AuctionDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        AuctionDetailView(userViewModel: UserViewModel(), auctionProductViewModel: AuctionProductViewModel())
+        NavigationStack {
+            AuctionDetailView(userViewModel: UserViewModel(), auctionProductViewModel: AuctionProductViewModel())
+        }
     }
 }
---
+++ Infantory/Infantory/View/Auction/SubView/AuctionItemImage.swift
@@ -0,0 +1,26 @@
+//
+//  AuctionItemImge.swift
+//  Infantory
+//
+//  Created by 변상필 on 10/3/23.
+//
+
+import SwiftUI
+
+struct AuctionItemImage: View {
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { _ in
+                Image("Influencer1")
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+#Preview {
+    AuctionItemImage()
+}
