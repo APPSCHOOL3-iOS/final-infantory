6af68f828e3b57660b3bd209e93d9ea63d18013e
Bongjooheon <112856133+Bongjooheon@users.noreply.github.com>
2023-10-05 16:28:39 +0900

    Feat: ApplyMainView에 플로팅버튼 추가

--- Infantory/Infantory/View/Apply/ApplyMainView.swift
+++ Infantory/Infantory/View/Apply/ApplyMainView.swift
@@ -10,13 +10,73 @@ import SwiftUI
 struct ApplyMainView: View {
     
     @StateObject var applyViewModel: ApplyProductViewModel = ApplyProductViewModel()
+    @ObservedObject var userViewModel: UserViewModel = UserViewModel()
     
     var body: some View {
-        NavigationStack {
-            VStack {
+        if userViewModel.user.isInfluencer != UserType.influencer {
+            NavigationStack {
+                ZStack {
+                    VStack {
+                        NavigationLink {
+                            ApplyRegistrationView()
+                        } label: {
+                            Text("임시")
+                        }
+                        Divider()
+                        ApplyProductListView(applyViewModel: applyViewModel)
+                        Divider()
+                    }
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            NavigationLink(destination: EmptyView()) {
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundColor(.black)
+                            }
+                        }
+                    }
+                    FloatingButton(action: {
+                        print("push ok")
+                    }, icon: "plus")
+                }
+            }
+        } else {
+            NavigationStack {
+                ZStack {
+                    VStack {
+                        Divider()
+                        ApplyProductListView(applyViewModel: applyViewModel)
+                        Divider()
+                    }
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            NavigationLink(destination: EmptyView()) {
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundColor(.black)
+                            }
+                        }
+                    }
+                    FloatingButton(action: {
+                        print("push ok")
+                    }, icon: "plus")
+                }
+            }
+        }
+    }
+}
+struct FloatingButton: View {
+    let action: () -> Void
+    let icon: String
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
                 NavigationLink {
                     ApplyRegistrationView()
                 } label: {
+<<<<<<< Updated upstream
                     Text("임시")
                 }
                 
@@ -31,6 +91,16 @@ struct ApplyMainView: View {
                         Image(systemName: "magnifyingglass")
                             .foregroundColor(.black)
                     }
+=======
+                    Image(systemName: icon)
+                        .font(.system(size: 25))
+                        .foregroundColor(.white)
+                        .frame(width: 60, height: 60)
+                        .background(Color.infanMain)
+                        .cornerRadius(30)
+                        .shadow(radius: 10)
+                        .offset(x: -25, y: -25)
+>>>>>>> Stashed changes
                 }
             }
             
