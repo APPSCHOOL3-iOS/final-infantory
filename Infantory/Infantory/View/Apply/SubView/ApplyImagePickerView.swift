c2b5f42aad89dc2695634b7a9e1ec182dab974fc
kempt007 <133959724+kempt007@users.noreply.github.com>
2023-10-06 15:17:00 +0900

    Feat: ApplyItemImageView, AuctionItemImage 파일 삭제

--- Infantory/Infantory/View/Apply/SubView/ApplyImagePickerView.swift
+++ Infantory/Infantory/View/Apply/SubView/ApplyImagePickerView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import Photos
+import PhotosUI
 import UIKit
 
 struct ApplyImagePickerView: View {
@@ -14,6 +14,7 @@ struct ApplyImagePickerView: View {
     @Binding var selectedImageNames: [String]
     @State private var isImagePickerPresented = false
     @State private var isGalleryPermissionGranted = false
+    var configuration = PHPickerConfiguration()
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
@@ -35,7 +36,8 @@ struct ApplyImagePickerView: View {
                             
                             RoundedRectangle(cornerRadius: 10)
                                 .stroke(Color.infanGray, lineWidth: 2)
-                                .frame(width: 100, height: 100)                        }
+                                .frame(width: 100, height: 100)
+                        }
                     }
                 }
                 ForEach(selectedImages, id: \.self) { image in
@@ -53,7 +55,6 @@ struct ApplyImagePickerView: View {
                                     return
                                 }
                                 selectedImages.remove(at: index)
-                                selectedImageNames.remove(at: index)
                             } label: {
                                 Image(systemName: "xmark.square.fill")
                                     .foregroundColor(.black)
@@ -63,10 +64,12 @@ struct ApplyImagePickerView: View {
             }
         }
         .sheet(isPresented: $isImagePickerPresented) {
-            MultiPhotoPickerView(selectedImages: $selectedImages, selectedImageNames: $selectedImageNames)
+            let configuration = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+            MultiPhotoPickerView(configuration: configuration
+                                 , selectedImages: $selectedImages, isPresented: $isImagePickerPresented)
         }
     }
-
+    
     func requestGalleryPermission() {
         PHPhotoLibrary.requestAuthorization { status in
             DispatchQueue.main.async {
@@ -81,48 +84,88 @@ struct ApplyImagePickerView: View {
 }
 
 struct MultiPhotoPickerView: UIViewControllerRepresentable {
+    let configuration: PHPickerConfiguration
     @Binding var selectedImages: [UIImage]
-    @Binding var selectedImageNames: [String]
-    @Environment(\.presentationMode) private var presentationMode
+    @Binding var isPresented: Bool
     
-    func makeUIViewController(context: UIViewControllerRepresentableContext<MultiPhotoPickerView>) -> UIImagePickerController {
-        let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
-        picker.allowsEditing = false
-        picker.sourceType = .photoLibrary
-        return picker
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        let controller = PHPickerViewController(configuration: configuration)
+        controller.delegate = context.coordinator
+        return controller
     }
     
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<MultiPhotoPickerView>) {}
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
-    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-        let parent: MultiPhotoPickerView
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        private let parent: MultiPhotoPickerView
         
         init(_ parent: MultiPhotoPickerView) {
             self.parent = parent
         }
         
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-            if let imageURL = info[.imageURL] as? URL {
-                let selectedImageNames = imageURL.lastPathComponent
-                parent.selectedImageNames.append(selectedImageNames)
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            parent.isPresented = false // Set isPresented to false because picking has finished.
+            
+            for result in results {
+                result.itemProvider.loadObject(ofClass: UIImage.self) { (object, error) in
+                    if let image = object as? UIImage {
+                        DispatchQueue.main.async {
+                            self.parent.selectedImages.append(image)
+                        }
+                    }
+                }
             }
-            if let selectedImage = info[.originalImage] as? UIImage {
-                parent.selectedImages.append(selectedImage)
-                //                print(parent.selectedImages)
-            }
-            parent.presentationMode.wrappedValue.dismiss()
-        }
-        
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            parent.presentationMode.wrappedValue.dismiss()
         }
     }
 }
+
+//struct MultiPhotoPickerView: UIViewControllerRepresentable {
+//    @Binding var selectedImages: [UIImage]
+//    @Binding var selectedImageNames: [String]
+//    @Environment(\.presentationMode)
+//
+//    func makeUIViewController(context: UIViewControllerRepresentableContext<MultiPhotoPickerView>) -> UIImagePickerController {
+//        let picker = UIImagePickerController()
+//        picker.delegate = context.coordinator
+//        picker.allowsEditing = false
+//        picker.sourceType = .photoLibrary
+//        return picker
+//    }
+//
+//    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<MultiPhotoPickerView>) {}
+//
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(self)
+//    }
+//
+//    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+//        let parent: MultiPhotoPickerView
+//
+//        init(_ parent: MultiPhotoPickerView) {
+//            self.parent = parent
+//        }
+//
+//        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+//            if let imageURL = info[.imageURL] as? URL {
+//                let selectedImageNames = imageURL.lastPathComponent
+//                parent.selectedImageNames.append(selectedImageNames)
+//            }
+//            if let selectedImage = info[.originalImage] as? UIImage {
+//                parent.selectedImages.append(selectedImage)
+//                //                print(parent.selectedImages)
+//            }
+//            parent.presentationMode.wrappedValue.dismiss()
+//        }
+//
+//        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//            parent.presentationMode.wrappedValue.dismiss()
+//        }
+//    }
+//}
 
 struct ApplyImagePickerView_Previews: PreviewProvider {
     static var previews: some View {
