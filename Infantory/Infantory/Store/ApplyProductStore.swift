cd960abfce037f0e0b3ef9a134ee89e340581bc0
Jiyoung Ahn <wl950204@naver.com>
2023-10-11 11:28:12 +0900

    Fix: 응모뷰에서 진행필터 마감순을 디폴트로 수정

--- Infantory/Infantory/Store/ApplyProductStore.swift
+++ Infantory/Infantory/Store/ApplyProductStore.swift
@@ -60,6 +60,7 @@ final class ApplyProductStore: ObservableObject {
         switch filter {
         case .inProgress:
             selectedFilter = .inProgress
+            progressSelectedFilter = .deadline
             filteredProduct = applyProduct.filter {
                 $0.applyFilter == .inProgress
             }.sorted { $0.endRemainingTime < $1.endRemainingTime }
