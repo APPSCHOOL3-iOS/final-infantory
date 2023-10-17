faaa9d5fa6baab8dd38fc2f26c7d20f65710f3dd
seonghun <96768451+Ggurup@users.noreply.github.com>
2023-10-17 16:18:03 +0900

    Feat: 런치스크린, 색깔 에셋 수정, 폰트 추가 (#112)
    * Feat: 커스텀 폰트 추가, 런치스크린 뷰 추가
    
    * Refactor: 다크모드 색상 대응 에셋 수정

--- Infantory/Infantory/Extension/Font+Extension.swift
+++ Infantory/Infantory/Extension/Font+Extension.swift
@@ -9,32 +9,32 @@ import SwiftUI
 
 extension Font {
     /// 28, regular
-    static let infanTitle = Font.system(size: 28, weight: .regular)
-    
+    static let infanTitle = Font.custom("SUIT-Regular", size: 28)
+
     /// 28, bold
-    static let infanTitleBold = Font.system(size: 28, weight: .bold)
-    
+    static let infanTitleBold = Font.custom("SUIT-Bold", size: 28)
+
     /// 22, regular
-    static let infanTitle2 = Font.system(size: 22, weight: .regular)
-    
+    static let infanTitle2 = Font.custom("SUIT-Regular", size: 22)
+
     /// 22, bold
-    static let infanTitle2Bold = Font.system(size: 22, weight: .bold)
-    
+    static let infanTitle2Bold = Font.custom("SUIT-Bold", size: 22)
+
     /// 16, semibold
-    static let infanHeadline = Font.system(size: 16, weight: .semibold)
-    
+    static let infanHeadline = Font.custom("SUIT-SemiBold", size: 16)
+
     /// 16, bold
-    static let infanHeadlineBold = Font.system(size: 16, weight: .bold)
-    
+    static let infanHeadlineBold = Font.custom("SUIT-Bold", size: 16)
+
     /// 16, regular
-    static let infanBody = Font.system(size: 16, weight: .regular)
-    
+    static let infanBody = Font.custom("SUIT-Regular", size: 16)
+
     /// 13, regular
-    static let infanFootnote = Font.system(size: 13, weight: .regular)
-    
+    static let infanFootnote = Font.custom("SUIT-Regular", size: 13)
+
     /// 13, bold
-    static let infanFootnoteBold = Font.system(size: 13, weight: .bold)
-    
+    static let infanFootnoteBold = Font.custom("SUIT-SemiBold", size: 13)
+
     /// 11, regular
-    static let infanCaption = Font.system(size: 11, weight: .regular)
+    static let infanCaption = Font.custom("SUIT-Regular", size: 11)
 }
