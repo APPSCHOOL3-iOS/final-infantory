4a8cb6045a02cd72478ff5a86a889225f4131580
Bongjooheon <112856133+Bongjooheon@users.noreply.github.com>
2023-10-18 17:22:57 +0900

    Feat: MyPaymentView 데이터 연동, MyPaymentsDetailView 상세보기버튼 수정 중 (#122)
    * Feat: MyPaymentView 수정
    
    * Feat: MyPaymentListView, DetailView 데이터 연동
    
    * Feat: MyPaymentsListView 에 네비게이션바 적용
    
    * Feat: MyPaymentView 수정 후 연동
    
    * Feat: MyPaymentsDetailView 꺽새 수정 중
    
    ---------
    
    Co-authored-by: seonghun <96768451+Ggurup@users.noreply.github.com>

--- Infantory/Infantory/Model/PaymentInfo.swift
+++ Infantory/Infantory/Model/PaymentInfo.swift
@@ -8,11 +8,11 @@
 import Foundation
 import FirebaseFirestoreSwift
 
-struct PaymentInfo: Codable {
+struct PaymentInfo: Codable, Identifiable {
     var id: UUID?
     var userId: String
-    var auctionProduct: AuctionProduct?
-    var applyProduct: ApplyProduct?
+    var auctionProduct: AuctionProduct? = nil
+    var applyProduct: ApplyProduct? = nil
     var address: Address
     var deliveryRequest: DeliveryMessages
     var deliveryCost: Int
@@ -22,4 +22,28 @@ struct PaymentInfo: Codable {
         case door, securityOffice, call, directMessage
         var id: Self {self}
     }
+    enum ProductType {
+        case auction
+        case apply
+    }
+    
+    var type: ProductType {
+        if auctionProduct != nil {
+            return .auction
+        } else {
+            return .apply
+        }
+    }
 }
+
+#if DEBUG
+extension PaymentInfo {
+    static let dummy =  PaymentInfo(userId: "",
+                                    address: Address.init(address: "",
+                                                          zonecode: "",
+                                                          addressDetail: ""),
+                                    deliveryRequest: .door,
+                                    deliveryCost: 3000,
+                                    paymentMethod: .accountTransfer)
+}
+#endif
